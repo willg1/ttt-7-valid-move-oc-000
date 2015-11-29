07b0cf37a84c47cdcf9e92c46f4e6ade1e13627d
willg1 <williamgalloway@optimum.net>
2015-11-29 08:03:02 +0000

    nitrous-backup at Sun Nov 29 08:03:02 UTC 2015

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -9,19 +9,3 @@ def valid_move?(board, position)
   end
 end
 
-=begin
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def position_taken?(board, position)
-  position = position - 1
-  return position
-end
-
-def position_taken?(board, position)
-  #position = position - 1
-  if board[position] == " " || board[position] == "" || board[position] == nil
-     return false
-  else board[position] == "X" || board[position] == "O"
-     return true
-  end
-end
-=end
